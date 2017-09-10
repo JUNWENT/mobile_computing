f38acbe75d160028c26a0fae63be23ead7d754bc
mwei11 <m.wei4@student.unimelb.edu.au>
2017-09-11 01:10:25 +1000

    initial commit

---
+++ HealthTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthTracker
+//
+//  Created by Mingyan Wei on 11/9/17.
+//  Copyright © 2017 Mingyan Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
