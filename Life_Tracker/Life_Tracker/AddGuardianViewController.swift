792fa3ecfdcdc1a5a6071313879c6c30096f1698
junwenz <junwenz@student.unimelb.edu.au>
2017-10-03 00:15:08 +1100

    add dependent connect

--- Life_Tracker/Life_Tracker/AddGuardianViewController.swift
+++ Life_Tracker/Life_Tracker/AddGuardianViewController.swift
@@ -12,6 +12,8 @@ class AddGuardianViewController: UIViewController {
     
     @IBOutlet weak var guardianPhoneNumberTextField: UITextField!
     
+    @IBOutlet weak var guardianSecretPassword: UITextField!
+    
     var username:String?
     
     override func viewDidLoad() {
@@ -27,27 +29,52 @@ class AddGuardianViewController: UIViewController {
     
     @IBAction func userPassedOnAdd(_ sender: Any) {
         let guardianPhoneNumber = guardianPhoneNumberTextField.text
+        let secretPassword = guardianSecretPassword.text
+        username = UserDefaults.standard.object(forKey: "DependentUsername") as? String
         let client = MSClient(applicationURLString: "https://life-tracker.azurewebsites.net")
         let table = client.table(withName: "UserRelationship")
-        let tableUser = client.table(withName: "userData")
+
         
-        if(guardianPhoneNumber?.isEmpty)!{
+        if((guardianPhoneNumber?.isEmpty)! || (secretPassword?.isEmpty)!){
             displayAlertMessage(useMessage: "You must enter an phone number.")
             return
         }
-        // store data to server
-        username = UserDefaults.standard.object(forKey: "DependentUsername") as! String
-        let itemToInsert = ["guardian": guardianPhoneNumber, "dependent": username, "read":false,"accept":false,"complete": false, "__createdAt": Date()] as [String : Any]
         
-        tableUser.read{ (result, error) in
+        table.read{ (result,error) in
             if let err = error {
                 self.displayAlertMessage(useMessage: "Please check you network and try again.")
                 return
-                    print("ERROR ", err)
-            } else if let items = result?.items {
+                    print ("ERROR",err)
+            }else if let items = result?.items{
                 for item in items {
-                    if (item["phoneNumber"] as? String == guardianPhoneNumber && item["complete"] as! Bool == false){
-                        // store data to server
+                    if(item["id"]as? String == guardianPhoneNumber && item["dependent"]as? String == self.username && item["password"]as? String == secretPassword && item["match"]as? Bool == false){
+                        table.update(["id": guardianPhoneNumber, "match":true, "complete": false]) { (result, error) in
+                            if let err = error {
+                                print("ERROR ", err)
+                            } else  {
+                                print("update match")
+                            }
+                        }
+                        self.displayNotificationMessage(useMessage: "You have successfully connect with your guardian.")
+                        print ("match dependent and guardian")
+                    } else if ((item["id"]as? String == guardianPhoneNumber && item["dependent"]as? String == self.username && item["match"]as? Bool == true) || (item["id"]as? String == self.username && item["guardian"]as? String == guardianPhoneNumber && item["match"]as? Bool == true)){
+                        self.displayAlertMessage(useMessage: "You have already connect with this guardian")
+                        return
+                    } else if (item["id"]as? String == guardianPhoneNumber && item["dependent"]as? String == self.username && item["password"]as? String != secretPassword && item["match"]as? Bool == false){
+                        self.displayAlertMessage(useMessage: "The guardian phone number or password are not correct. Please check and try agian.")
+                        return
+                    } else if(item["id"]as? String == self.username && item["guardian"]as? String == guardianPhoneNumber && item["match"]as? Bool == false){
+                        table.update(["id": self.username, "password":secretPassword, "complete": false]) { (result, error) in
+                            if let err = error {
+                                print("ERROR ", err)
+                                print ("has error")
+                            } else  {
+                                print("changing password!")
+                            }
+                        }
+                        self.displayNotificationMessage(useMessage: "You have set your secret password. Please wait for your guardian to connect you.")
+                    } else {
+                        let itemToInsert = ["id": self.username, "dependent": self.username,"guardian":guardianPhoneNumber,"match":false,"password":secretPassword, "complete": false, "__createdAt": Date()] as [String : Any]
                         UIApplication.shared.isNetworkActivityIndicatorVisible = true
                         table.insert(itemToInsert) {
                             (item, error) in
@@ -57,28 +84,13 @@ class AddGuardianViewController: UIViewController {
                                 print("Error: " + (error! as NSError).description)
                             }
                         }
-                        // display sucessful reigster message
-                        let alert = UIAlertController(title:"COMFIRMATION",message:"You have sent your request to the guardian. Please wait for him to accept.",preferredStyle:UIAlertControllerStyle.alert)
-                        
-                        let okAction = UIAlertAction(title:"OK",style:UIAlertActionStyle.default){
-                            action in
-                            self.dismiss(animated: true, completion: nil)
-                        }
-                        alert.addAction(okAction)
-                        self.present(alert, animated: true, completion: nil)
+                        self.displayNotificationMessage(useMessage: "You have set your secret password. Please wait for your guardian to connect you.!!!!")
+    
                     }
                 }
-                
-                self.displayAlertMessage(useMessage: "This guardian does not exist.")
-                return
             }
         }
-        
-        
-        
-        
-        
-        
+
     }
     
     //display alert message
@@ -89,6 +101,13 @@ class AddGuardianViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    //display notification message
+    func displayNotificationMessage(useMessage:String){
+        let alert = UIAlertController(title:"Notification",message:useMessage,preferredStyle:UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title:"OK",style:UIAlertActionStyle.default,handler:nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
     
     /*
      // MARK: - Navigation
