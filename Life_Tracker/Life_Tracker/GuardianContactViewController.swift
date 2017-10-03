ad6f862c95591e4ef3055018992a75b718fcd461
junwenz <junwenz@student.unimelb.edu.au>
2017-10-03 20:30:36 +1100

    add guardian contact

---
+++ Life_Tracker/Life_Tracker/GuardianContactViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GuardianContactViewController.swift
+//  Life_Tracker
+//
+//  Created by junwenz on 2017/10/3.
+//  Copyright © 2017年 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class GuardianContactViewController: UIViewController {
+
+    @IBAction func UserPressedOnLogout(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
