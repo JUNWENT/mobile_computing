fda7f37ad6ca66267d31be26150c9d67813285e3
张子一 <zhangziyi@192-168-1-6.tpgi.com.au>
2017-10-03 13:15:57 +1100

    Update:Reset UI

---
+++ Life_Tracker/Life_Tracker/DependentResetPasswordViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DependentResetPasswordViewController.swift
+//  Life_Tracker
+//
+//  Created by 张子一 on 03/10/2017.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class DependentResetPasswordViewController: UIViewController {
+
+    @IBOutlet weak var userExistingPassword:UITextView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
