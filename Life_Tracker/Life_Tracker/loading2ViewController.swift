69d1aa5b51665eb1968cbbbeed8e7321625384fd
张子一 <zhangziyi@192-168-1-6.tpgi.com.au>
2017-10-06 11:03:34 +1100

    Update:loading page

--- Life_Tracker/Life_Tracker/loading2ViewController.swift
+++ Life_Tracker/Life_Tracker/loading2ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import SwiftGifOrigin
 
 class loading2ViewController: UIViewController {
-
+    
     var username:String?
-
+    
     
     override func viewDidLoad() {
         //loading.startAnimating()
         super.viewDidLoad()
-
+        let imageData = try! Data(contentsOf: Bundle.main.url(forResource: "main3-iloveimg-cropped", withExtension: "gif")!)
+        let advTimeGif = UIImage.gif(data: imageData)
+        let loadingImage = UIImageView(image:advTimeGif)
+        loadingImage.frame = CGRect (x: 10.0, y: 222.0, width: 290.0, height: 202.0)
+        
+        //CGRect()
+        
+        view.addSubview(loadingImage)
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,15 +44,15 @@ class loading2ViewController: UIViewController {
         }
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
